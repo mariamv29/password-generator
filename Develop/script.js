8fb7929037084a0773dcd4683fa9da479941add1
Maria Mireles <mireles335@gmail.com>
2021-05-30 16:26:36 -0500

    final code check

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,7 @@
-// Get references to the #generate element
+// Get references to the #generate element- assignment code here
 var generateBtn = document.querySelector("#generate");
-
-generateBtn.addEventListener("click", generatePassword);
-
+generateBtn.addEventListener("click", writePassword);
 ///Define characters that the password can contain
-
 var upperCaseCharacters = [
   "A",
   "B",
@@ -87,17 +84,11 @@ var symbolsCharacters = [
   "_",
   ".",
 ];
-
-//Password variable declaration
-var passwordText = document.querySelector("#password");
-
 //Generate password function
-
 function generatePassword() {
   var confirmLength = prompt(
     "How many characters would you like our password to contain?"
   );
-
   // validate prompt answer
   while (confirmLength <= 8 || confirmLength >= 128) {
     alert(
@@ -105,64 +96,57 @@ function generatePassword() {
     );
     return;
   }
-
-  //Different password criterias
+  var passwordArray = userSelectionOptions();
+  var randomPassword = generateRandomPassword(passwordArray, confirmLength);
+  return randomPassword;
+}
+//if in the user selection options, symbols is true then push symbols array, if boolean is true then add global symbols array into a new array
+function userSelectionOptions() {
+  var combinedArray = [];
   var confirmNumericCharacters = confirm(
     "select OK to include numeric characters"
   );
-
   var confirmSymbolsCharacters = confirm(
     "Select OK to include symbol characters"
   );
-
-  var confirmupperCaseCharacters = confirm(
+  var confirmUpperCaseCharacters = confirm(
     "Select OK to include upper case characters"
   );
-
-  var confirmlowerCaseCharacers = confirm(
+  var confirmLowerCaseCharacters = confirm(
     "Select OK to include lower case characters"
   );
-  //variable to contain users selections
-  var userSelectionOptions = {
-    length: confirmLength,
-    symbols: confirmSymbolsCharacters,
-    numbers: confirmNumericCharacters,
-    upperCase: confirmupperCaseCharacters,
-    lowerCase: confirmlowerCaseCharacers,
-  };
-
-  return userSelectionOptions;
-}
-
-//if in the user selection options, symbols is true then push symbols array, if boolean is true then add global symbols array into a new array
-
-function writePassword() {
-  var userSelectionOptions = generatePassword();
-  var combinedArray = [];
-  if (userSelectionOptions.symbols) {
-    combinedArray = combinedArray.concat(userSelectionOptions.symbols);
-  }
-  if (userSelectionOptions.numbers) {
-    combinedArray = combinedArray.concat(userSelectionOptions.numbers);
-  }
-  if (userSelectionOptions.upperCase) {
-    combinedArray = combinedArray.concat(userSelectionOptions.upperCase);
-  }
-  if (userSelectionOptions.lowerCase) {
-    combinedArray = combinedArray.concat(userSelectionOptions.lowerCase);
-  }
-  if (userSelectionOptions.length) {
-    combinedArray = combinedArray.concat(userSelectionOptions.length);
-  }
-  writePassword();
-  // loop over based on number on length
-
-  for (var i = 0; i < combinedArray.length; i++) {
-    if (!userSelectionOptions[i].combinedArray) {
-      window.alert("Your password has been created: combinedArray.concat[i]");
+  if (confirmNumericCharacters === true) {
+    for (x of numericCharacters) {
+      combinedArray.push(x);
     }
   }
+  if (confirmSymbolsCharacters === true) {
+    for (x of symbolsCharacters) {
+      combinedArray.push(x);
+    }
+  }
+  if (confirmUpperCaseCharacters === true)
+    for (x of upperCaseCharacters) {
+      combinedArray.push(x);
+    }
+  if (confirmLowerCaseCharacters === true)
+    for (x of lowerCaseCharacters) {
+      combinedArray.push(x);
+    }
+  return combinedArray;
 }
-// save to its own var then print to DOM element
+function generateRandomPassword(array, length) {
+  var selections = "";
+  for (var index = 0; index < length; index++) {
+    var result = array[Math.floor(Math.random() * array.length)];
+    selections += result;
+  }
+  return selections;
+}
 
-passwordText.value = password;
+//   userSelectionOptions.push(selections);
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
